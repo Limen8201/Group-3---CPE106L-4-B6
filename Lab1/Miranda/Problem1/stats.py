3dba41af5f12af1e5f2c3fa1cb67b5a9bc9a00ee
Jojazzzzzzzzzzz <traevesmc@gmail.com>
2024-12-14 15:43:35 +0800

    Update stats.py

--- Lab1/Miranda/Problem1/stats.py
+++ Lab1/Miranda/Problem1/stats.py
@@ -1,1 +1,16 @@
+import numpy as np
+from scipy import stats
+def mean_func(num):
+  print('Mean: ',np.mean(num))
 
+def med_func(num):
+  print('Meadian: ',np.median(num))
+
+def mode_func(num):
+  print(stats.mode(num))
+
+lst = [1,2,3,4,5,6,7,8,9,10]
+
+mean_func(lst)
+med_func(lst)
+mode_func(lst)
