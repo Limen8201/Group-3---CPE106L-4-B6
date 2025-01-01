34019f251257ff670e770d61297e05be6df236a4
Crisson <91372910+Limen8201@users.noreply.github.com>
2025-01-01 14:53:54 +0800

    Update LR2_2.py

--- LAB 2/John Michael Bejo/Problem 2/LR2_2.py
+++ LAB 2/John Michael Bejo/Problem 2/LR2_2.py
@@ -1,17 +1,19 @@
 import linecache 
 
-fileName = input("Insert a filename (name.txt): ")
+fileName = input("Insert a filename: ")
 file = open(fileName, "r")
 
 i=0
 for x in file:
+    print(i+1,")", x, end="")
     i=i+1
 
+print("")
+lineNum = i
 print(fileName, "has", i, "line")
-
-lineNum = 0
-lineNum = int(input("Please choose a line to print: "))
-if lineNum == 0:
-    exit()
+while lineNum != 0:
+    lineNum = int(input("Please choose a line to print (Type 0 to stop): "))
+    print(linecache.getline(fileName, lineNum))
+    if lineNum == 0:
+        exit()
   
-print(linecache.getline(fileName, lineNum) )
